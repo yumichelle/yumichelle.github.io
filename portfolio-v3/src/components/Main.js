bae5fd3d1349db5c9188a6124bbf510f63573327
mic <mic@mics-Book.local>
2023-08-05 12:53:20 -0400

    set up version 3

---
+++ portfolio-v3/src/components/Main.js
@@ -0,0 +1,23 @@
+import logo from '../images/logo.svg';
+import '../css/Main.css';
+
+function App() {
+  return (
+      <header className="Main-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="Main-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+  );
+}
+
+export default App;
